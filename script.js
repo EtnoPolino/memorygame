469c8911be63cbaf4402de1d518e57af6f8e8839
EtnoPolino <einshoch66@gmail.com>
2022-04-14 04:28:14 +0200

    redo

--- script.js
+++ script.js
@@ -25,46 +25,33 @@ const carte = [
     }
 ];
 
-/* we sort randomly the images */
+
 carte.sort(() => {
     return 0.5 - Math.random();
 });
 
+
+/****************************************************
+ *              Declaration of variables
+ * **************************************************/
 const container = document.querySelector('.container');
-const cacheClicked = [];
 
-
+/****************************************************
+ *  Here we're going to create our cache card
+ * it supposed to hide all our images 
+ ****************************************************/
 for(let i = 0; i < carte.length; i++){
-    const cacheImage = document.createElement('img');
-    cacheImage.setAttribute('src', 'images/blankgreen.jpg');
-    cacheImage.setAttribute('data-id', i);
-    container.appendChild(cacheImage);
-    
-    cacheImage.addEventListener('click', flipCacheImage);
+    const cacheImg = document.createElement('img');
+    cacheImg.setAttribute('src', 'images/blankgreen.jpg');
+    container.appendChild(cacheImg);
+
+    cacheImg.addEventListener('click', flipCard);
 }
 
+/***
+ * 
+ */
 
-function flipCacheImage(e){
-    const carteId = this.getAttribute('data-id');
-    cacheClicked.push(carte[carteId]);
-    
-} 
+function flipCard(){
 
-
-/*
-    Comment mettre une image random derrière un cache image chaque fois qu'on clique ??
-
-        - j'attribue un id à chaque cache image,
-        - à chaque id sera associé une image random de carte quand on aura cliqué dessus,
-            en d'autre terme, il faut que je fasse correspondre les id
-            par exemple, si je clique sur le cache id 3, ça considera que j'ai cliqué sur un id de la carte 
-
-*/
-
-/* 
-        fonction flip cacheImage
-
-    - je défini un id de mon tableau de carte équivalent à mon cache, : 
-    - je défini un tableau de la carte choisit dont je vais attribuer un id.. carteClicked[cardID].push(carte) 
-
-*/
+}
